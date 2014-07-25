b1d534bd1cc1fb81cd8698e1d29899911b218099
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2014-07-24 23:46:32 -0400

    replace ugly unsigned chars with pretty uint_8

--- src/sort.c
+++ src/sort.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include "rng.h"
 #include "sort.h"
 
@@ -9,10 +10,10 @@ bool rd_sorted(void *base, size_t nmemb, size_t size,
   if (size == 0) {
     return true;
   }
-  register unsigned char *a = (base), *b = (base) + size,
-                         *end = (base) + (size * nmemb);
+  register uint8_t *a = (base), *b = (base) + size,
+                   *end = (base) + (size * nmemb);
   while (b != end) {
-    if (cmp(a, b) > 0) {
+    if (cmp((void *)a, (void *)b) > 0) {
       return false;
     }
     a = b, b += size;
@@ -21,13 +22,13 @@ bool rd_sorted(void *base, size_t nmemb, size_t size,
 }
 
 void rd_knuth_shuffle(void *base, size_t nmemb, size_t size, rd_rng *r) {
-  unsigned char swap[size];
+  uint8_t swap[size];
   int x;
   for (size_t i = nmemb - 1; i > 0; --i) {
     x = rd_rng_int(r, i + 1);
-    memcpy(&swap[0], (void*)((unsigned char*)base + x * size), size);
-    memcpy((void*)((unsigned char*)base + (x * size)), (void*)((unsigned char*)base + (i * size)), size);
-    memcpy((void*)((unsigned char*)base + (i * size)), &swap[0], size);
+    memcpy(&swap[0], (void*)((uint8_t*)base + x * size), size);
+    memcpy((void*)((uint8_t*)base + (x * size)), (void*)((uint8_t*)base + (i * size)), size);
+    memcpy((void*)((uint8_t*)base + (i * size)), &swap[0], size);
   }
 }
 
@@ -50,15 +51,15 @@ void rd_stdbogosort(void *base, size_t nmemb, size_t size,
 
 void rd_isort(void *base, size_t nmemb, size_t size,
               int (*cmp)(const void *, const void *)) {
-  unsigned char swap[size];
+  uint8_t swap[size]; // Would need more vars with a swap macro...
   for (int i = 1; i < nmemb; ++i) {
-    memcpy(&swap[0], (void*)((unsigned char*)base + i * size), size);
+    memcpy(&swap[0], (void *)((uint8_t *)base + i * size), size);
     int j = i;
     for (; j > 0 && cmp(&swap, base + (j - 1) * size); --j) {
-      memcpy((void*)((unsigned char *)base + j * size),
-             (void*)((unsigned char *)base + (j - 1) * size), size);
+      memcpy((void*)((uint8_t *)base + j * size),
+             (void*)((uint8_t *)base + (j - 1) * size), size);
     }
-    memcpy((void*)((unsigned char *)base + j * size), &swap[0], size);
+    memcpy((void*)((uint8_t *)base + j * size), &swap[0], size);
   }
 }
 
