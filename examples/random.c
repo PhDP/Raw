6498874825925e50298ae8780c25b4689dede463
Philippe Desjardins-Proulx <philippe.d.proulx@gmail.com>
2015-01-08 22:43:33 -0500

    add unsigned ints to the example

--- examples/random.c
+++ examples/random.c
@@ -10,6 +10,10 @@ int main() {
     // Generates double-precision numbers in the [0, 1) range.
     printf("%f\n", rd_rng_double(&r));
   }
+  printf("\nRandom 32-bit unsigned integers:\n");
+  for (int i = 0; i < 28; ++i) {
+    printf("%u\n", rd_rng_uint(&r));
+  }
   return EXIT_SUCCESS;
 }
 
